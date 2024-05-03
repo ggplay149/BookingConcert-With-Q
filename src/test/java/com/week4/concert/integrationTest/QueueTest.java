2ab8ad138e25dacc1392901b3d88640f722cdf45
syou149149 <syou149149@gmail.com>
2024-05-03 10:41:47 +0900

    feat : Reservation lock redis adapting

--- src/test/java/com/week4/concert/integrationTest/QueueTest.java
+++ src/test/java/com/week4/concert/integrationTest/QueueTest.java
@@ -22,29 +22,7 @@ public class QueueTest {
     private QueueUseCase queueUseCase;
 
 
-    @Test
-    @Transactional
-    @DisplayName("Ongoing 48명일 때 waiting 테이블 최우선 2명추가 / 이후 추가된 2명 waiting 테이블에서 soft delete(done) 처리 확인")
-    void queueUpdate() {
-
-        //given
-        for (long i = 1; i <= 48; i++) {
-            ongoingSerivce.insert(i);
-        }
-        for (long i = 49; i <= 49 + 10; i++) {
-            waitingService.insert(i);
-        }
-
-        //when
-        queueUseCase.queueUpdate();
-
-        //then
-        assertThat(ongoingSerivce.countOngoing()).isEqualTo(50);
-        assertThat(ongoingSerivce.check(50L).status()).isEqualTo("Ongoing");
-        assertThat(waitingService.selectTopN(2).get(0).userId()).isEqualTo(51);
-    }
-
-    @Test
+     @Test
     @DisplayName(" waiting or ongoing 테이블에 둘다 없으면 대기열입장 가능")
     void insertQueue1() {
         //given
