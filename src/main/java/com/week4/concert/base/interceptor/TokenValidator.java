3c1909c62a38cbc0f30f333169c360ef4d5db307
syou149149 <syou149149@gmail.com>
2024-05-13 09:51:53 +0900

    feat : 이벤트 처리전 로직 정리 : 토큰검증 수정

--- src/main/java/com/week4/concert/base/interceptor/TokenValidator.java
+++ src/main/java/com/week4/concert/base/interceptor/TokenValidator.java
@@ -1,5 +1,6 @@
 package com.week4.concert.base.interceptor;
 
+import com.week4.concert.domain.queue.QueueReader;
 import com.week4.concert.domain.queue.QueueService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
@@ -9,10 +10,12 @@ import org.springframework.transaction.annotation.Transactional;
 @RequiredArgsConstructor
 public class TokenValidator {
 
-    private final QueueService queueService;
+    private final QueueReader queueReader;
 
     @Transactional
     public void valid(Long userId) {
-
+        if(!queueReader.checkUserStatus(userId,"Active")){
+            throw new RuntimeException("활성화된 유저가 아닙니다.");
+        }
     }
 }
