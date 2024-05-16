91b2df4002b2c93a3303d231ef4db532b5cfdd58
syou149149 <syou149149@gmail.com>
2024-05-16 14:29:42 +0900

    feat : applicationEvent 처리
    - PaymentEvent 작성
    - PaymentEventListener 작성
    - PaymentEventPublisher 작성
    - 외부api 호출, 대기열 Active권한 삭제 비동기 이벤트 처리

--- src/main/java/com/week4/concert/application/PaymentUseCase.java
+++ src/main/java/com/week4/concert/application/PaymentUseCase.java
@@ -4,6 +4,7 @@ import com.week4.concert.domain.concert.Concert;
 import com.week4.concert.domain.concert.ConcertService;
 import com.week4.concert.domain.payment.PaymentService;
 import com.week4.concert.domain.payment.event.PaymentEvent;
+import com.week4.concert.domain.payment.event.PaymentEventPublisher;
 import com.week4.concert.domain.reservation.ReservationService;
 import com.week4.concert.domain.user.UserService;
 import lombok.RequiredArgsConstructor;
@@ -15,11 +16,11 @@ import org.springframework.transaction.annotation.Transactional;
 @RequiredArgsConstructor
 public class PaymentUseCase {
 
-    private final ApplicationEventPublisher applicationEventPublisher;
     private final PaymentService paymentService;
     private final ReservationService reservationService;
     private final ConcertService concertService;
     private final UserService userService;
+    private final PaymentEventPublisher paymentEventPublisher;
 
     @Transactional
     public String pay(String reservationNumber, Long userId) {
@@ -36,7 +37,7 @@ public class PaymentUseCase {
 
         concertService.increaseReservationCount(reservedConcert.id());
 
-        applicationEventPublisher.publishEvent(new PaymentEvent(reservationNumber, reservedConcert, userId));
+        paymentEventPublisher.publishEvent(new PaymentEvent(reservationNumber, reservedConcert, userId));
 
         return "정상 결제되었습니다. 예약이 확정되었습니다.";
     }
