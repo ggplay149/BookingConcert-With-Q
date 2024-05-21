698a2df6e17c2b504f66842de529810c323ec178
ggplay149 <ggplay149@gmail.com>
2024-05-21 20:38:58 +0900

    feat : event 정리

--- src/main/java/com/week4/concert/domain/payment/event/PaymentEventListener.java
+++ src/main/java/com/week4/concert/domain/payment/event/PaymentEventListener.java
@@ -4,11 +4,13 @@ import com.week4.concert.admin.MessageService;
 import com.week4.concert.domain.queue.QueueService;
 import com.week4.concert.domain.reservation.ReservationService;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Component;
 import org.springframework.transaction.event.TransactionPhase;
 import org.springframework.transaction.event.TransactionalEventListener;
 
+@Slf4j
 @Component
 @RequiredArgsConstructor
 public class PaymentEventListener {
@@ -27,12 +29,14 @@ public class PaymentEventListener {
     @Async
     public void removeActiveUser(PaymentEvent event) {
         queueService.removeActiveUser(event.userId());
+        log.info(" :: 대기열 해제 완료 ::");
     }
 
     @TransactionalEventListener(phase = TransactionPhase.AFTER_COMMIT)
     @Async
     public void removeTemporaryReservation(PaymentEvent event) {
         reservationService.removeTemporaryReservation(event.reservationNumber());
+        log.info(" :: 임시예약 해제 완료 ::");
     }
 }
 
